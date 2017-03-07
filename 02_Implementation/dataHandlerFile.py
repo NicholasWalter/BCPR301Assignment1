d5caa68045836f193c474ad436b983d2926cb7e1
NicholasWalter <NicholasWalter@web.de>
2017-03-08 10:41:35 +1300

    redoing dataHandlerFile.py with OOP

--- 02_Implementation/dataHandlerFile.py
+++ 02_Implementation/dataHandlerFile.py
@@ -11,66 +11,46 @@ import os
 import config
 import employee
 import inputConverter as IC
+"""
+this module offers the functionality of reading from and writing to the
+save file
+"""
+
+# python imports
 import util
 
+# project imports
+from dataHandlerAbstract import DataHandlerAbstract
 
-def save_employees(to_save, target_file):
-    """
-    saves the supplied list of employees to the supplied save file
-    """
-    raise NotImplementedError
+class DataHandlerFile(DataHandlerAbstract):
+    def __init__(self, file_path):
+        Data
+        self.file_path = file_path
 
-def get_all_employees(source_file = "XXX"):
-    """
-    reads all employees from the configured csv file
-    @params: -
-    @return:
-        [[Employee],[String]] containing Employee objects and error string
-    """
-    if source_file == "XXX":
-        source_file = util.get_config()["datasource"]
+    def save_employees(self, employees):
+        raise NotImplementedError
 
-    result = []
-    errors = []
+    def save_employee(self, employee):
+        raise NotImplementedError
 
-    neccessary_keys = ["empid", "gender", "sales", "bmi", "salary", "birthday",
-                        "age"]
+    def get_all_employees(self):
+        """
+        reads the file this object was initiated with and returns all contained
+        employees as Employee objects in a list
+        """
+        raise NotImplementedError
+        with open(self.file) as source:
+            for line in source:
+                
 
-    read_first_line = False
-    with open(source_file) as file:
-        line_counter = 1
-        for line in file:
-            if not read_first_line:             # ignore first line
-                read_first_line = True
-                continue
-            try:
-                split = line.split(",")         # split by comma
-                attributes = {}
+    def get_employee(self, emp_id):
+        raise NotImplementedError
 
-                # for each of the neccessary keys, get conversion, then add
-                # to attributes
-                for i in range(len(neccessary_keys)):
-                    key = neccessary_keys[i]
-                    attributes[key] = IC.convert_input(split[i], key)
+    def employee_exists(self, employee):
+        raise NotImplementedError
 
-                attributes["age"] = utils.calculate_age(attributes["birthday"])
-                new_age = util.calculate_age(new_birthday)
-                results.append(employee.create_employee())
-            except ValueError as verr:
-                errors.append("Error on line {}: ".format(line_counter)+
-                                str(verr))
-            line_counter += 1
-    return[result, errors]
-
-def check_if_employee_exists(to_check):
-    """
-    checks if the supplied employee already exists in the csv file
-    @params:
-        to_check: employee to check
-    @return:
-        true if the employee exists, otherwise false
-    """
-    raise NotImplementedError
+    def update_employee(self, employee):
+        raise NotImplementedError
 
 ################################################################################
 ## Testing functionality                                                      ##
