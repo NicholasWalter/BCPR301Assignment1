807607bb7f9067f7411d920cc79246fa65df3751
NicholasWalter <NicholasWalter@web.de>
2017-03-14 15:19:58 +1300

    finished implementation of dataHandlerSerial.py (not tested yet)

--- 02_Implementation/dataHandlerSerial.py
+++ 02_Implementation/dataHandlerSerial.py
@@ -1,6 +1,7 @@
 """
 this module offers the functionality of serializing objects and restoring them
-to memory.
+to memory. All objects will be stored in files with their employee id as name
+and .emp as extension.
 """
 
 # python imports
@@ -15,7 +16,7 @@ import IOHelper as IO
 class DataHandlerSerial(DataHandlerAbstract):
     def __init__(self, save_path = "XXX"):
         DataHandlerAbstract.__init__(self)
-        if save_path == "XXX":
+        if save_path == "XXX":              # default path is "01_datasources/"
             path = os.path.dirname(os.path.realpath(__file__))
             add_path = "01_datasources/"
             save_path = os.path.join(path, add_path)
@@ -28,9 +29,14 @@ class DataHandlerSerial(DataHandlerAbstract):
 
         for f in all_files:
             try:
-                employees.append(pickle.load(open(f, "rb")))
+                new_emp = pickle.load(open(f, "rb"))
+                if type(new_emp).__name__ == "Employee":
+                    employees.append(new_emp)
+                else:
+                    err_text = "File {} does not contain an employee object."
+                    IO.stdErr(err_text.format(f))
             except:
-                err_text = "Error reading file: {}"
+                err_text = "Could not read file: {}"
                 IO.stdErr(err_text.format(f))
         return employees
 
@@ -45,9 +51,19 @@ class DataHandlerSerial(DataHandlerAbstract):
                 IO.stdErr(err_text.format(emp.employee_id))
 
     def update_employee(self, employee):
-        raise NotImplementedError
+        os.chdir(self._save_path)
+        target_file = "{}.emp".format(employee.employee_id)
+        if not os.path.isfile(target_file):
+            IO.stdErr("Could not find the supplied employee to update")
+            return
+        self.save_employees([employee])
 
     def delete_employees(self, employees):
-        raise NotImplementedError
+        os.chdir(self._save_path)
+        for emp in employees:
+            try:
+                os.remove("{}.emp".format(emp.employee_id))
+            except:
+                IO.stdErr("Could not delete employee {}".format(emp.employee_id))
 
 a = DataHandlerSerial()
